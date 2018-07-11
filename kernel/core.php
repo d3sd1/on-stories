9ea99e920b19b465c3a00c0690913ad23a45c8ce
Desdi <andreigarciacuadra@gmail.com>
2018-07-11 13:53:11 +0200

    Initialized project

---
+++ kernel/core.php
@@ -0,0 +1,8 @@
+<?php
+/* Langs */
+$lang = parse_ini_file('langs/es.lang');
+/* Config */
+$config = parse_ini_file('config.conf');
+/* Database */
+$db = require('class/database.php');
+?>
---
+++ onstories.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-09-2016 a las 00:19:28
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 7.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `onstories`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` text NOT NULL,
+  `previous_content` text NOT NULL,
+  `content` longtext NOT NULL,
+  `author` text NOT NULL,
+  `comments_nmb` int(11) NOT NULL,
+  `categories` text NOT NULL,
+  `date` int(11) NOT NULL,
+  `extra_data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
